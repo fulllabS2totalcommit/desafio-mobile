f7fc0b3cc1c17cdd9ea72d429a814022b1c93c24
Vinicius Santos Ricci <vinicius.ricci4@gmail.com>
2017-08-26 15:27:08 -0300

    Initial Commit

---
+++ MobFiq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobFiq
+//
+//  Created by Vinicius on 26/08/17.
+//  Copyright © 2017 Vinicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
